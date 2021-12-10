962349414af7c5ccd22ea4d1ac2aaae6baea9c6d
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-12-11 00:32:16 +0100

    Add user controller

---
+++ ClothesShopAPI/ClothesShopAPI/Controllers/UserController.cs
@@ -0,0 +1,92 @@
+﻿using ClothesShopAPI.Models.DTO.PostDTO;
+using ClothesShopAPI.Models.Others;
+using ClothesShopAPI.Services.LoginService;
+using ClothesShopAPI.Services.RegisterService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothesShopAPI.Controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IRegisterService _registerService;
+        private readonly ILoginService _loginService;
+        public UserController(IRegisterService registerService, ILoginService loginService)
+        {
+            _registerService = registerService;
+            _loginService = loginService;
+        }
+        [Route("register")]
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> RegisterNewUser([FromBody]CreateUserDTO newuser)
+        {
+            try
+            {
+                await _registerService.CreateNewUser(newuser);
+                return Ok();
+            }
+            catch(MyException ex)
+            {
+                return StatusCode(ex.StatusCode, ex.StatusMessage);
+            }
+        }
+        [Route("confirm/email")]
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            try
+            {
+                await _registerService.ConfirmEmail(userId, token);
+                return Ok();
+            }
+            catch(MyException ex)
+            {
+                return StatusCode(ex.StatusCode, ex.StatusMessage);
+            }
+        }
+        [Route("login")]
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> LogIn([FromBody]LoginDTO loginmodel)
+        {
+            try
+            {
+                var token = await _loginService.SignIn(loginmodel);
+                return Ok(token);
+            }
+            catch(MyException ex)
+            {
+                return StatusCode(ex.StatusCode, ex.StatusMessage);
+            }
+        }
+        [Route("logout")]
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> SignOut(string refreshtoken)
+        {
+            try
+            {
+                await _loginService.SignOut(refreshtoken);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [Route("test")]
+        [Authorize]
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("Authorize page");
+        }
+    }
+}
