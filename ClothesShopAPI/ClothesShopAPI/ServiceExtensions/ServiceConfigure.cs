5a106b3b6b60cb64c0b692ba173b83ae030fd121
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-12-13 19:27:01 +0100

    Update ServiceConfigure.cs

--- ClothesShopAPI/ClothesShopAPI/ServiceExtensions/ServiceConfigure.cs
+++ ClothesShopAPI/ClothesShopAPI/ServiceExtensions/ServiceConfigure.cs
@@ -1,6 +1,15 @@
-﻿using ClothesShopAPI.Services.EmailService;
+using ClothesShopAPI.Queries.BasketQueries;
+using ClothesShopAPI.Queries.ClothQueries;
+using ClothesShopAPI.Queries.CommentQueries;
+using ClothesShopAPI.Queries.OpinionQueries;
+using ClothesShopAPI.Queries.SearchQueries;
+using ClothesShopAPI.Queries.UserQueries;
+using ClothesShopAPI.Queries.OrderQueries;
+using ClothesShopAPI.Services.CurrentLoggedInUser;
+using ClothesShopAPI.Services.EmailService;
 using ClothesShopAPI.Services.LoginService;
 using ClothesShopAPI.Services.RegisterService;
+using ClothesShopAPI.Services.SessionService;
 using ClothesShopAPI.Services.TokenServices;
 using ClothesShopAPI.Services.ValidationService;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,6 +29,18 @@ namespace ClothesShopAPI.ServiceExtensions
             services.AddScoped<ILoginService, ClassicLoginService>();
             services.AddScoped<ITokenService, JwtTokenService>();
             services.AddScoped<IValidationService, ExistingUserValidationService>();
+            services.AddScoped<IGetClothQueries, ClothQueries>();
+            services.AddScoped<ICommentQueries, CommentQueries>();
+            services.AddScoped<IOpinionQueries, OpinionQueries>();
+            services.AddScoped<IAdressQueries, UserQueries>();
+            services.AddScoped<ICurrentLoggedInUser, CurrentLoggedInUserService>();
+            services.AddScoped<ISearchQueries, SearchQueries>();
+            services.AddScoped<IClothSessionService, ClothSession>();
+            services.AddScoped<IEmailService, ResetPasswordEmailService>();
+            services.AddScoped<IPasswordQueries, PasswordQueries>();
+            services.AddScoped<IBasketQueries, UserBasketQueries>();
+			services.AddScoped<>(IMakeOrderQueries, LoggedUserOrderQueries);
+			services.AddScoped<>(ISeeOrderQueries, LoggedUserOrderQueries);
         }
     }
 }
