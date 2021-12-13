cbe0fedca95fae5d94d1fa876cd44b30c876b7a5
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-12-13 19:16:34 +0100

    Update ClothOpinionsEntity.cs

--- ClothesShopAPI/ClothesShopAPI/Database/Entities/ClothOpinionsEntity.cs
+++ ClothesShopAPI/ClothesShopAPI/Database/Entities/ClothOpinionsEntity.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,16 @@ namespace ClothesShopAPI.Database.Entities
     public class ClothOpinionsEntity
     {
         public int Id { get; set; }
+        [Required]
         public uint OpinionSum { get; set; }
+        [Required]
         public uint UserSum { get; set; }
         public int Cloth_Id { get; set; }
         public virtual ClothEntity Cloth { get; set; }
-        public string User_Id { get; set; }
-        public virtual UserEntity User { get; set; }
+        public virtual ICollection<UserOpinionEntity> UserOpinioned { get; set; }
+        public ClothOpinionsEntity()
+        {
+            UserOpinioned = new List<UserOpinionEntity>();
+        }
     }
 }
