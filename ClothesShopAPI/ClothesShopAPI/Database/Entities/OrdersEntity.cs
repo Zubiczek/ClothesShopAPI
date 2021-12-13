74c33ad15b318a226377e5015a1d2c1ac7085184
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-12-13 19:18:22 +0100

    Update OrdersEntity.cs

--- ClothesShopAPI/ClothesShopAPI/Database/Entities/OrdersEntity.cs
+++ ClothesShopAPI/ClothesShopAPI/Database/Entities/OrdersEntity.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,14 +10,18 @@ namespace ClothesShopAPI.Database.Entities
     {
         public string Id { get; set; }
         public DateTime CreatedOn { get; set; }
+        [DefaultValue(false)]
         public bool IsItPaid { get; set; }
+        [DefaultValue(false)]
         public bool IsItSend { get; set; }
+        [DefaultValue(false)]
         public bool IsItDelivered { get; set; }
         public string User_Id { get; set; }
         public virtual UserEntity User { get; set; }
         public virtual ICollection<OrderClothesEntity> Clothes { get; set; }
         public OrdersEntity()
         {
+            CreatedOn = DateTime.UtcNow;
             Clothes = new List<OrderClothesEntity>();
         }
     }
