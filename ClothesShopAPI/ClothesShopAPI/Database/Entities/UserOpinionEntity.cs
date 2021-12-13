5689a585fecfff0f5970c138efd4af01c64c3862
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-12-13 19:20:22 +0100

    Add UserOpinion Entity

---
+++ ClothesShopAPI/ClothesShopAPI/Database/Entities/UserOpinionEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothesShopAPI.Database.Entities
+{
+    public class UserOpinionEntity
+    {
+        public int Opinion_Id { get; set; }
+        public virtual ClothOpinionsEntity Opinion { get; set; }
+        public string User_Id { get; set; }
+        public virtual UserEntity User { get; set; }
+        [Required]
+        [Range(1, 5)]
+        public uint Value { get; set; }
+    }
+}
