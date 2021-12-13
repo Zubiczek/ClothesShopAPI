ba750ba06eafc0fcb50d7d252d63ed214f1c741e
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-12-13 19:17:47 +0100

    Update GenderEntity.cs

--- ClothesShopAPI/ClothesShopAPI/Database/Entities/GenderEntity.cs
+++ ClothesShopAPI/ClothesShopAPI/Database/Entities/GenderEntity.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace ClothesShopAPI.Database.Entities
     public class GenderEntity
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(15)]
         public string Name { get; set; }
         public virtual ICollection<ClothEntity> Clothes { get; set; }
         public GenderEntity()
