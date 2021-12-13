6a03d89044e7afde06b0c199a5efeb82e6dcf5ba
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-12-13 19:20:07 +0100

    Update UserEntity.cs

--- ClothesShopAPI/ClothesShopAPI/Database/Entities/UserEntity.cs
+++ ClothesShopAPI/ClothesShopAPI/Database/Entities/UserEntity.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,19 +10,22 @@ namespace ClothesShopAPI.Database.Entities
     public class UserEntity : IdentityUser
     {
         public DateTime CreatedOn { get; set; }
+        [MaxLength(50)]
         public string Address { get; set; }
+        [MaxLength(30)]
         public string City { get; set; }
+        [MaxLength(6)]
         public string ZipCode { get; set; }
         public virtual OrdersEntity Order { get; set; }
-        public virtual ICollection<ClothOpinionsEntity> Opinions { get; set; }
         public virtual ICollection<ClothCommentsEntity> Comments { get; set; }
         public virtual ICollection<UserBasketEntity> BasketClothes { get; set; }
+        public virtual ICollection<UserOpinionEntity> Opinions { get; set; }
         public UserEntity()
         {
             CreatedOn = DateTime.UtcNow;
-            Opinions = new List<ClothOpinionsEntity>();
             Comments = new List<ClothCommentsEntity>();
             BasketClothes = new List<UserBasketEntity>();
+            Opinions = new List<UserOpinionEntity>();
         }
     }
 }
