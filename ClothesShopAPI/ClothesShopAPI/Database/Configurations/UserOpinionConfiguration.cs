17a19d3432d7e615d1c9fede3bc31ac0947e06e4
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-12-13 19:22:02 +0100

    Add User Opinion Entity Configuration

---
+++ ClothesShopAPI/ClothesShopAPI/Database/Configurations/UserOpinionConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ClothesShopAPI.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothesShopAPI.Database.Configurations
+{
+    public class UserOpinionConfiguration : IEntityTypeConfiguration<UserOpinionEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserOpinionEntity> builder)
+        {
+            builder.HasKey(x => new { x.Opinion_Id, x.User_Id });
+            builder.HasOne(x => x.Opinion).WithMany(x => x.UserOpinioned).HasForeignKey(x => x.Opinion_Id);
+            builder.HasOne(x => x.User).WithMany(x => x.Opinions).HasForeignKey(x => x.User_Id);
+        }
+    }
+}
