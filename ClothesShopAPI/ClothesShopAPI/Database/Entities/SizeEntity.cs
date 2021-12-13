4ff62cc50d09e1876efc5c91f583af5e2b099739
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-12-13 19:19:17 +0100

    Update SizeEntity.cs

--- ClothesShopAPI/ClothesShopAPI/Database/Entities/SizeEntity.cs
+++ ClothesShopAPI/ClothesShopAPI/Database/Entities/SizeEntity.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace ClothesShopAPI.Database.Entities
     public class SizeEntity
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(3)]
         public string Size { get; set; }
         public virtual ICollection<UserBasketEntity> Baskets { get; set; }
         public virtual ICollection<ClothSizeEntity> ClothSizes { get; set; }
